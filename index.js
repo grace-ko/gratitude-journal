e6d531447895d69afe143e938ce129a298c32843
grace-ko <gracekodes@gmail.com>
2021-08-09 12:39:42 -0700

    corrected port config

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 require('./app/routes.js')(app);
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
+
   console.log(`listening on ${PORT}`);
 });
