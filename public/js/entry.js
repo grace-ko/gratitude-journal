1a6b470e63210ff7aab17bf3d7ca6712f083381b
grace-ko <gracekodes@gmail.com>
2021-09-16 15:56:13 -0700

    implemented delete function from screen and db also to hide delete button when list is empty

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,10 +1,19 @@
 jQuery(document).ready(function(){
 	jQuery('.entry-list').on('click','.delete-button',function(e){
 		e.preventDefault();
-    const value = $("input[type='checkbox']:checked");
-    
-    console.log(value[0].id);
-    console.log(value[1].id);
-    console.log("clicked");
+    const checkedEntry = $("input[type='checkbox']:checked");
+		const entryItem = $(checkedEntry).parent();
+		console.log($('.individual-entry'))
+		for (var i = 0; i < entryItem.length; i++) {
+			$(entryItem[i]).remove();
+			$.ajax({
+				method: 'DELETE',
+				url: '/entry/'+ entryItem[i].id
+			}).done(function(data){
+				if($('.individual-entry').length===0) {
+					$('.delete-button').hide();
+				}
+			});
+		}
 	});
 });
